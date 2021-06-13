920925f2a17ca8292548a6353db979d4905a8373
not populated <jan.rydt@northumbria.ac.uk>
2021-06-13 09:16:40 +0100

    some adjustment of ATS settings in Ua

--- PTDC_ARCHER2_999/ua_custom/Ua2D_InitialUserInput.m
+++ PTDC_ARCHER2_999/ua_custom/Ua2D_InitialUserInput.m
@@ -9,10 +9,10 @@ CtrlVar.TotalNumberOfForwardRunSteps=10e10; % an arbitrary large number
 CtrlVar.TotalTime=UserVar.UaMITgcm.runTime;
 CtrlVar.Restart=1;
 
-CtrlVar.dt = 1e-3;
+CtrlVar.dt=1e-3;
 CtrlVar.RestartTime=0; 
 CtrlVar.ResetTime=1;
-CtrlVar.ResetTimeStep=1;    % perhaps this has to be reconsidered if model has issues converging
+CtrlVar.ResetTimeStep=0;    % perhaps this has to be reconsidered if model has issues converging
 
 % Parallel options
 %myCluster = parcluster('local') ;  
@@ -33,9 +33,9 @@ CtrlVar.AdaptMesh=1;
 % timestepping
 CtrlVar.ATStimeStepTarget = UserVar.UaMITgcm.ATStimeStepTarget; 
 CtrlVar.dtmin = 1e-10;
-CtrlVar.ATStimeStepFactorUp=2 ;
-CtrlVar.ATStimeStepFactorDown=5 ;
-CtrlVar.ATSTargetIterations=2;
+CtrlVar.ATStimeStepFactorUp=1.5;
+CtrlVar.ATStimeStepFactorDown=5;
+CtrlVar.ATSTargetIterations=4;
 
 CtrlVar.InitialDiagnosticStep=1;
 CtrlVar.TestUserInputs=0;
