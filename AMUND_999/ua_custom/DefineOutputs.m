9ce83f0d732b381d1c94707655a5e0b2e0fa2d47
janderydt <jan.rydt@northumbria.ac.uk>
2022-08-09 14:44:06 +0100

    changes function name for DefineOutputs.m

--- AMUND_999/ua_custom/DefineOutputs.m
+++ AMUND_999/ua_custom/DefineOutputs.m
@@ -1,4 +1,4 @@
-function UserVar=DefineOutputs_v2(UserVar,CtrlVar,MUA,BCs,F,l,GF,InvStartValues,InvFinalValues,Priors,Meas,BCsAdjoint,RunInfo)
+function UserVar=DefineOutputs(UserVar,CtrlVar,MUA,BCs,F,l,GF,InvStartValues,InvFinalValues,Priors,Meas,BCsAdjoint,RunInfo)
 
 
 v2struct(F);
